de4f0b87254cef3762ea49a23e41b3d51cf65527
umxziz <umxziz@student.usm.my>
2023-08-14 10:47:55 +0800

    create component for one movie
    add css and svh file and write code for single movie to display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
-import React from "react";
+import { useEffect } from "react";
+
+import './App.css';
+import SearchIcon from './search.svg'
+
+const API_URL = 'http://www.omdbapi.com?apikey=9a01717c';
+
+const movie1 = {
+    "Title": "Spiderman",
+    "Year": "1990",
+    "imdbID": "tt0100669",
+    "Type": "movie",
+    "Poster": "N/A"
+}
 
 const App = () => {
+
+    const searchMovie = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovie('Spiderman');
+    }, []);
+
     return (
-        <>
-            <h1>App</h1>
-            <h2>Test</h2>
-        </>
+        <div className="app">
+            <h1>Netflix</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value="Superman"
+                    onChange={() => { }}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => { }}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://vis.placeholder.com/400'} alt={movie1.Title} />
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
